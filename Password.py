398dd4ceb9fca9fbe5e322c0ae1f04a1eb4e01ef
Jonny <93822303+JonnyAsh@users.noreply.github.com>
2022-01-24 22:10:57 +0800

    Update Password.py

--- Password.py
+++ Password.py
@@ -4,7 +4,7 @@ password=input("What is your password?")
 
 while password!="A.334ssd":
     print("Error")
-    time.sleep(10)
+    time.sleep(9)
     password=input("What is your password?") #Possible duplication 
 
 print("Password correct")
